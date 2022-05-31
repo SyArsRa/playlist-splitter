ecbb7c6aefccbe6abaffa2cf48274c5ed8e4bd10
Syed Arsal Rahman <arsalazfar@hotmail.com>
2022-05-31 18:05:12 +0500

    working around disabled button completed

--- webpages/script.js
+++ webpages/script.js
@@ -1,35 +1,18 @@
 const button = document.getElementById("Slice");
 
-function inputcheck() {
-  input = document.getElementById("playid");
-  if (input != null) {
-    if (input.value == undefined){
-      button.disabled = true;
-      console.log("h")
-    } else {
-      button.disabled = false;
-      console.log("y")
-    }
-  }
-}
-
-
-
 button.addEventListener("mouseover",function(event){
-  inputcheck()
   const div = document.getElementsByClassName("title");
   div[0].className = "titlehover";
 })
 
 button.addEventListener("mouseout",function(event){
-  inputcheck()
   const div = document.getElementsByClassName("titlehover");
   div[0].className = "title";
 })
 
 button.addEventListener("click",function(event) {
   if (button.className.includes("pressed")) {
-    document.getElementById("playid").submit();
+    document.getElementById("playlistForm").submit();
   } else {
     var title = document.getElementsByClassName("titlecenter");
     title[0].className = "moveleft";
@@ -39,17 +22,29 @@ button.addEventListener("click",function(event) {
       var form = document.createElement("form");
       form.setAttribute("method", "post");
       form.setAttribute("action", "");
-      form.setAttribute("id", "playid");
       form.setAttribute("align", "center");
       form.setAttribute("style", "color:#28282B");
       form.setAttribute("class", "formcenter");
+      form.setAttribute("id","playlistForm")
       var playlist = document.createElement("input");
       playlist.setAttribute("type", "text");
-      playlist.setAttribute("name", "playid");
+      playlist.setAttribute("name", "playlistId");
+      playlist.setAttribute("id", "playid")
       playlist.setAttribute("placeholder", "Enter Playlist URL");
       form.appendChild(playlist);
       button.innerHTML = "Submit!";
       document.getElementById("ti").appendChild(form);
+    }
+  }
+})
+
+document.getElementById("hoverbtn").addEventListener("mouseover", function(){
+  input = document.getElementById("playid");
+  if (input != null) {
+    if (input.value.trim().length == 0 ){
+      button.disabled = true;
+    } else {
+      button.disabled = false;
     }
   }
 })
